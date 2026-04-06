75204ff686aa984b6de334c8f53e9dfc6715a2cf
Manogna2304 <manognaav3270@gmail.com>
2026-04-07 01:22:54 +0530

    Refactor cycle length calculation in CyclePredictor
    Refactored cycle length calculation and removed standard deviation calculation.

--- cycle_predictor.py
+++ cycle_predictor.py
@@ -1,23 +1,21 @@
-import numpy as np
+import pandas as pd
 from datetime import timedelta
-
 
 class CyclePredictor:
 
-    def _get_cycle_lengths(self, dates):
-        return [(dates[i] - dates[i-1]).days for i in range(1, len(dates))]
+    def __init__(self):
+        self.df = pd.read_csv("data/cycle.csv")
 
     def predict_next(self, dates):
 
         if len(dates) < 2:
             return {"predicted_length": 28}
 
-        lengths = self._get_cycle_lengths(dates)
-        avg = int(np.mean(lengths))
+        lengths = [(dates[i] - dates[i-1]).days for i in range(1, len(dates))]
+        avg = int(sum(lengths) / len(lengths))
 
         return {
             "predicted_length": avg,
             "next_predicted_date": max(dates) + timedelta(days=avg),
-            "avg_cycle": avg,
-            "std_dev": round(np.std(lengths), 1)
+            "avg_cycle": avg
         }

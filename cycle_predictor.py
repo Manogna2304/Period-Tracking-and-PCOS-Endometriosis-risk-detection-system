12e2a3a2a267e0c702439884eb9df282e2671e7d
Manogna2304 <manognaav3270@gmail.com>
2026-04-07 09:36:16 +0530

    Refactor CyclePredictor methods for indentation

--- cycle_predictor.py
+++ cycle_predictor.py
@@ -3,19 +3,19 @@ from datetime import timedelta
 
 class CyclePredictor:
 
-    def __init__(self):
-        self.df = pd.read_csv("data/cycle.csv")
+  def __init__(self):
+      self.df = pd.read_csv("data/cycle.csv")
 
-    def predict_next(self, dates):
+  def predict_next(self, dates):
 
-        if len(dates) < 2:
-            return {"predicted_length": 28}
+      if len(dates) < 2:
+          return {"predicted_length": 28}
 
-        lengths = [(dates[i] - dates[i-1]).days for i in range(1, len(dates))]
-        avg = int(sum(lengths) / len(lengths))
+      lengths = [(dates[i] - dates[i-1]).days for i in range(1, len(dates))]
+      avg = int(sum(lengths) / len(lengths))
 
-        return {
-            "predicted_length": avg,
-            "next_predicted_date": max(dates) + timedelta(days=avg),
-            "avg_cycle": avg
-        }
+      return {
+          "predicted_length": avg,
+          "next_predicted_date": max(dates) + timedelta(days=avg),
+          "avg_cycle": avg
+      }
